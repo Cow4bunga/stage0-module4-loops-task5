42d0488be2905915c648693571456c036eb9daf4
Ivan Levitsky <yavanyaprivet@gmail.com>
2022-09-04 12:14:25 +0300

    done

--- src/main/java/school/mjc/stage0/loops/task5/Cross.java
+++ src/main/java/school/mjc/stage0/loops/task5/Cross.java
@@ -2,6 +2,16 @@ package school.mjc.stage0.loops.task5;
 
 public class Cross {
     public void printCross(int sideLength) {
-
+        int mid = sideLength / 2;
+        for (int i = 0; i < sideLength; i++) {
+            for (int j = 0; j < sideLength; j++) {
+                if (i != mid && j != mid) {
+                    System.out.print(" ");
+                    continue;
+                }
+                System.out.print(8);
+            }
+            System.out.println();
+        }
     }
 }
--- src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
+++ src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
@@ -2,6 +2,60 @@ package school.mjc.stage0.loops.task5;
 
 public class Hourglass {
     public void printHourglassOfGivenSize(int height) {
+        int counter = height;
+        int mid = height / 2;
+        for (int i = 0; i < mid; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+
+            for (int k = 0; k < counter; k++) {
+                System.out.print(8);
+            }
+
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+            counter -= 2;
+            System.out.println();
+        }
+
+        if(height%2==0){
+            counter+=2;
+            for (int i = mid; i < height; i++) {
+                for (int j = height - i - 1; j > 0; j--) {
+                    System.out.print(" ");
+                }
+
+                for (int k = 0; k < counter; k++) {
+                    System.out.print(8);
+                }
+
+                for (int j = height - i - 1; j > 0; j--) {
+                    System.out.print(" ");
+                }
+
+                counter += 2;
+                System.out.println();
+            }
+        }else {
+            for (int i = mid; i < height; i++) {
+                for (int j = height - i - 1; j > 0; j--) {
+                    System.out.print(" ");
+                }
+
+                for (int k = 0; k < counter; k++) {
+                    System.out.print(8);
+                }
+
+                for (int j = height - i - 1; j > 0; j--) {
+                    System.out.print(" ");
+                }
+
+                counter += 2;
+                System.out.println();
+            }
+        }
 
     }
 }
